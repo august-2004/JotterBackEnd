62e4de1ce986310ed0eee2ce57ee9959384defd9
Vanjinathan <115717134+august-2004@users.noreply.github.com>
2024-09-26 20:21:44 +0530

    index is updated to include passport settings and routes

--- api/index.js
+++ api/index.js
@@ -1,52 +1,65 @@
-/* eslint-disable no-multiple-empty-lines */
-/* eslint-disable linebreak-style */
 const express = require('express');
-const mongoose = require('mongoose');
-const noteData = require('./noteSchema');
+const session = require('express-session');
+const passport = require('passport');
 const cors = require('cors');
+const LocalStrategy = require('passport-local')
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt')
+require('dotenv').config();
 
+const RegisterRoute = require('../routes/RegisterRoute');
+const LoginRoute = require('../routes/LoginRoute');
+const SaveRoute = require('../routes/SaveRoute')
 
+const User = require('../models/userSchema')
 
 const app = express();
 const PORT = 3001;
-app.use(cors({
-    origin: "https://jotterapp.vercel.app",
-    
-},{
-    origin: "https://localhost:5173",
-    
-}));
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-mongoose.connect(process.env.MONGOACCESSS).then(()=>
-        console.log("Connected sucessfuly to the database")).catch((err)=>console.log(err));
+mongoose.connect(process.env.MONGOACCESSS)
+    .then(()=>console.log("Connected sucessfuly to the database"))
+    .catch((err)=>console.log(err));
 
-        app.use((req, res, next) => {
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            
-            next();
-          });
-app.get('/', (request,response)=>
-{
-     response.send(200,"The server works");
+
+app.use(session({ secret: process.env.SESSION_SECRET, resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+passport.use(new LocalStrategy.Strategy(
+    async (username, password, done) => {
+        const user = await User.findOne({ username });
+        if (!user) {
+            return done(null, false, { message: 'Incorrect username.' });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return done(null, false, { message: 'Incorrect password.' });
+        }
+        return done(null, user);
+    }
+));
+
+passport.serializeUser((user, done) => done(null, user.id));
+passport.deserializeUser(async (id, done) => {
+    const user = await User.findById(id);
+    done(null, user);
 });
 
-app.post('/postnote', async (req,res) => {
-    console.log(req.body);
-    res.header("Access-Control-Allow-Origin", "*");
-    const {heading , para} = req.body;
-    const newNote = await noteData.create(req.body);
-    await    newNote.save();
-    res.send("file Stored")
-    
-})
 
-app.get('/getnote', async (req,res) => {
-    console.log(req.body);
-    const all = await noteData.find();
-    res.send(all);
-    console.log( "sent data", all)
-})
+app.get('/', (request,response)=>
+    {
+        response.send(200,"The server works");
+    });
+
+app.use(RegisterRoute);
+app.use(LoginRoute);
+app.use(SaveRoute);
+
+
+
 
 app.listen(PORT, ()=>{
     console.log("Running in port 3001");
