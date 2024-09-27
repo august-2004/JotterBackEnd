86684f078c4101b90651c88617acf1631a8953b5
Vanjinathan <115717134+august-2004@users.noreply.github.com>
2024-09-27 22:18:26 +0530

    updated cors again

--- api/index.js
+++ api/index.js
@@ -42,9 +42,9 @@ app.use(session({
         collectionName: 'sessions',
         }),
     cookie: {
-            secure: false, 
+            secure: true, 
             maxAge: 1000*60*60*24,
-            sameSite: 'Lax',
+            sameSite: 'none',
             path: '/',
             httpOnly: true
         }
