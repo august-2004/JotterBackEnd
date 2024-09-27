444d3d1c8e907a723d70a26379fdaf98ff453bcd
Vanjinathan <115717134+august-2004@users.noreply.github.com>
2024-09-27 23:03:30 +0530

    cors again

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const PORT = 3001;
 const allowedOrigins = ["https://jotterapp.vercel.app"];
 
 app.use(cors({
-  origin: allowedOrigins,
+  origin: "https://jotterapp.vercel.app",
   credentials: true 
 }));
 app.use(express.json());
