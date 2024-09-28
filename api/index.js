915ef2f0dedcbcc4d4bffcf153388a718a6d294f
Vanjinathan <115717134+august-2004@users.noreply.github.com>
2024-09-28 15:24:41 +0530

    removed slash

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ const PORT = 3001;
 const allowedOrigins = ["https://jotterapp.vercel.app"];
 
 app.use(cors({
-  origin: "https://jotterapp.vercel.app/",
+  origin: "https://jotterapp.vercel.app",
   credentials: true 
 }));
 
